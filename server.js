7ad9a91d1b7bb14d412469a4a2df88c5cb0ae432
= <=>
2017-07-31 22:48:41 +0530

    Added About Page

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express') ;
 const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 const port = process.env.PORT || 8888;
 
 var app = express();
 app.get('/', (req, res) => {
-	res.render('footer.hbs', {
-		pageTitle: 'Sample', 
+	res.render('style.hbs', {
+		pageTitle: 'Home', 
+		author: 'Balaji', 
+		currentYear: new Date().getFullYear()
+	});
+});
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Us', 
 		author: 'Balaji', 
 		currentYear: new Date().getFullYear()
 	});
